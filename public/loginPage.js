3c35593d37b334d28733296b26a45fa8ec44dcbb
Denis B <bulavatsky@list.ru>
2023-09-16 12:33:25 +0300

    del ( ) response

--- public/loginPage.js
+++ public/loginPage.js
@@ -5,20 +5,20 @@ const userForm = new UserForm();
 // Функция обратного вызова для входа в личный кабинет
 userForm.loginFormCallback = (data) => {
   // Вызываем метод для отправки данных на сервер и обработки результата
-  ApiConnector.login(data, (response) => {
+  ApiConnector.login(data, response => {
     if (response.success) {
       // Если успешно, перезагружаем страницу
       location.reload();
     } else {
       // Если есть ошибка, устанавливаем сообщение об ошибке
       userForm.setLoginErrorMessage(response.error);
-    }
+    };
   });
 };
 
 // Функция обратного вызова для регистрации
 userForm.registerFormCallBack = (data) => {
-  ApiConnector.register(data, (response) => {
+  ApiConnector.register(data, response => {
     if (response.success) {
       location.reload();
     } else {
